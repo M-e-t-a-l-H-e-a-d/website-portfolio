d72d113588e1a79862174acabeba4b7eb5bd1e1d
Sergey_Feoktistov <bbldyk@gmail.com>
2024-11-21 19:59:38 +0300

    Реализовал выпадающее меню навигации по сайту в мобильной версии

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+// Выпадающее меню навигации по сайту в мобильной версии
+const dropdownBtn = document.getElementById("dropdown_button");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("dropdown__arrowIcon");
+
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+// Переключатель. Когда кликаем на кнопку, меню открывается/закрывается
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+// Закрываем меню при плике на какой-либо элемент DOM
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
+
+// Модальное окно подтверждения, что ссылка на телеграм скопирована (в футере)
 const telegramLink = "https://t.me/M_e_t_a_l_H_e_a_d";
 const dialogSuccessCopyTelegramLink = document.getElementById(
   "dialogSuccessCopyTelegramLink"
